0957e630dd20689aa6731b2269ab0b721583e7ff
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-06-24 20:20:20 -0500

    Added grade averaging do-while loop.

--- LoopPractice/Program.cs
+++ LoopPractice/Program.cs
@@ -6,10 +6,28 @@ namespace LoopPractice
     {
         public static void Main(string[] args)
         {
-            for(int i = 1; i <=20; i+=2)
+            Console.Write("How many test scores are you entering? ");
+            string input = Console.ReadLine();
+            int numberOfTestScores = int.Parse(input);
+            int scoreSum = 0;
+            int counter = 1;
+           
+            do
             {
-                Console.WriteLine(i + " is an odd number that is less than 20.");
-            }
+                Console.Write("Please enter the student score: ");
+                string studentScore = Console.ReadLine();
+                int scoreNum = int.Parse(studentScore);
+                scoreSum += scoreNum;
+                counter++;
+
+            } while (counter <= numberOfTestScores);
+            int testAverage = scoreSum / numberOfTestScores;
+            Console.WriteLine("The average of the test scores is: " + testAverage);
+
+            //for(int i = 1; i <=20; i+=2)
+            //{
+            //    Console.WriteLine(i + " is an odd number that is less than 20.");
+            //}
         }
     }
 }
