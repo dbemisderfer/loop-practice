a7886ecc0ea1e3a2cfb0b4a6b7db12783c41ad83
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-06-24 21:05:41 -0500

    Added students on the bus counter challenge.

--- LoopPractice/Program.cs
+++ LoopPractice/Program.cs
@@ -6,24 +6,53 @@ namespace LoopPractice
     {
         public static void Main(string[] args)
         {
-            Console.Write("How many test scores are you entering? ");
-            string input = Console.ReadLine();
-            int numberOfTestScores = int.Parse(input);
-            int scoreSum = 0;
-            int counter = 1;
-           
-            do
+            ///////////////////////////////
+            /////// People counter ////////
+            ///////////////////////////////
+            Console.WriteLine("Press ENTER to add to count. Enter any other key + ENTER to stop counting.");
+
+            int counter = 0;
+            bool finishedCounting = false;
+
+            while(!finishedCounting)
             {
-                Console.Write("Please enter the student score: ");
-                string studentScore = Console.ReadLine();
-                int scoreNum = int.Parse(studentScore);
-                scoreSum += scoreNum;
-                counter++;
+                string input = Console.ReadLine();
+                if(input == "")
+                {
+                    counter += 1;
+                    Console.WriteLine("{0} student(s) currently on the bus.", counter);
 
-            } while (counter <= numberOfTestScores);
-            int testAverage = scoreSum / numberOfTestScores;
-            Console.WriteLine("The average of the test scores is: " + testAverage);
+                } else
+                {
+                    finishedCounting = true;
+                }
+            }
+            Console.WriteLine("All " + counter + " students are on the bus.");
 
+            ///////////////////////////////
+            //////// Test scores //////////
+            ///////////////////////////////
+            //Console.Write("How many test scores are you entering? ");
+            //string input = Console.ReadLine();
+            //int numberOfTestScores = int.Parse(input);
+            //int scoreSum = 0;
+            //int counter = 1;
+           
+            //do
+            //{
+            //    Console.Write("Please enter the student score: ");
+            //    string studentScore = Console.ReadLine();
+            //    int scoreNum = int.Parse(studentScore);
+            //    scoreSum += scoreNum;
+            //    counter++;
+
+            //} while (counter <= numberOfTestScores);
+            //int testAverage = scoreSum / numberOfTestScores;
+            //Console.WriteLine("The average of the test scores is: " + testAverage);
+
+            ///////////////////////////////
+            //////// Odd numbers //////////
+            ///////////////////////////////
             //for(int i = 1; i <=20; i+=2)
             //{
             //    Console.WriteLine(i + " is an odd number that is less than 20.");
