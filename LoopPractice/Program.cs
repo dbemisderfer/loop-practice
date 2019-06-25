767424e85b3841b5150b8b74b9234d06c3ef9919
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-06-24 19:17:46 -0500

    Initial check-in of module LoopPractice

---
+++ LoopPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LoopPractice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for(int i = 1; i <=20; i+=2)
+            {
+                Console.WriteLine(i + " is an odd number that is less than 20.");
+            }
+        }
+    }
+}
